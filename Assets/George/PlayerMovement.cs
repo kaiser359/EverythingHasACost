edf3c7e77c4f673ca0d60c7718ff7e4a69ae56e3
Phobia <george.chmykhalo@gmail.com>
2026-03-10 17:51:23 -0700

    Light thingy and basic movement

---
+++ Assets/George/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+
+    public GlobalPlayerInfo gS;
+    Rigidbody2D rb;
+    Vector2 desiredVelocity;
+    private float moveSpeed;
+    void Awake()
+    {
+        moveSpeed = gS.moveSpeed;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Called by the Input System. Bind your action to `Move` with a Vector2 control (e.g., WASD or left stick).
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        rb.linearVelocity = ctx.ReadValue<Vector2>() * moveSpeed;
+    }
+}
