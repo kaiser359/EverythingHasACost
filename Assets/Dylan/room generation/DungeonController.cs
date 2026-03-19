070148fec77a0985fd42e26f4a3556966e151f49
MUKILTEO\1725375 <azenosnothere@gmail.com>
2026-03-19 10:22:49 -0700

    touchup

--- Assets/Dylan/room generation/DungeonController.cs
+++ Assets/Dylan/room generation/DungeonController.cs
@@ -1,4 +1,26 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Cooldown
+{
+    private readonly float cooldownTime;
+    private float lastUsedTime;
+
+    public Cooldown(float cooldownTime)
+    {
+        this.cooldownTime = cooldownTime;
+        this.lastUsedTime = -cooldownTime; // Initialize to allow immediate use
+    }
+    public bool IsReady()
+    {
+        return Time.time >= lastUsedTime + cooldownTime;
+    }
+
+    public void Use()
+    {
+        lastUsedTime = Time.time;
+    }
+}
 
 public class DungeonController : MonoBehaviour
 {
@@ -8,16 +30,29 @@ public class DungeonController : MonoBehaviour
     public int minRooms = 30;
     private RoomSpawner startSpawner;
 
+    private Cooldown regenerateCooldown = new(1f);
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startSpawner = GameObject.FindWithTag("StartPoint").GetComponent<RoomSpawner>();
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && regenerateCooldown.IsReady())
+        {
+            RegenerateDungeon();
+            regenerateCooldown.Use();
+        }
+    }
+
     public void RegenerateDungeon()
     {
         roomsSpawned = 0;
         enemiesSpawned = 0;
+
+        transform.GetChild(0).transform.GetChild(0).GetComponent<Tilemap>().ClearAllTiles();
 
         for (int i = 1; i < transform.childCount; i++)
         {
