b11b0b5ec84db64ce0bf59f61de2a5e099062d7f
MUKILTEO\1721488 <GerhmanSparrow>
2026-03-23 08:32:29 -0700

    no message

---
+++ Assets/Gabriel/LazerBOOM.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class LazerBOOM : MonoBehaviour
+{
+public float damage = 20f;
+public float timer = 0;
+    public Animation animata;
+    public Animator animator;
+
+    public void ActivateAbility()
+    {
+
+
+    }
+}
