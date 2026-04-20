fcc5cf801f7052a907640cffa2038e77bdfbaa10
MUKILTEO\1725219 <george.chmykhalo@gmail.com>
2026-04-20 10:07:33 -0700

    upgradesys

--- Assets/George/Upgrade.cs
+++ Assets/George/Upgrade.cs
@@ -8,7 +8,7 @@ public class Upgrade : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gS = FindAnyObjectByType<GlobalPlayerInfo>();
     }
 
     // Update is called once per frame
--- Assets/George/UpgradeScreen.cs
+++ Assets/George/UpgradeScreen.cs
@@ -7,7 +7,7 @@ public class UpgradeScreen : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UpgradeScreenUI.SetActive(false);
     }
 
     // Update is called once per frame
