d7f5e8fde264a1ef42a0c756510257ecc8890e97
MUKILTEO\1721488 <GerhmanSparrow>
2026-04-27 09:38:30 -0700

    animations

---
+++ Assets/Gabriel/Aninm/EnemyAnim.cs
@@ -0,0 +1,87 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class EnemyAnim : MonoBehaviour
+{
+    public Animator anim;
+
+    public float moveSpeed;
+    private Rigidbody2D rb;
+
+    private float x;
+    private float y;
+    private bool Possesed = false;
+    private float startposition;
+    private Vector2 input;
+    private bool Moving;
+    private Vector2 oldposition;
+    private Vector2 newposition;
+    void Start()
+    {
+    }
+    private void Update()
+    {
+
+
+        newposition = transform.position;
+
+
+        GetInput();
+
+        Animate();
+
+        startposition = Vector2.Distance(newposition, oldposition);
+        oldposition = transform.position;
+    }
+    private void FixedUpdate()
+    {
+       // rb.velocity = input * moveSpeed;
+    }
+
+    private void GetInput()
+    {
+        //Vector2 velocity = newposition - oldposition;
+
+        //input = new Vector2(x, y);
+        //input.Normalize();
+    }
+    private void Animate()
+    {
+        if (newposition != oldposition) //|| input.magnitude < -0.1f)
+        {
+            Moving = true;
+        }
+        else { Moving = false; }
+
+        if (newposition.x > oldposition.x && Mathf.Abs(newposition.x - oldposition.x) > Mathf.Abs(newposition.y - oldposition.y))
+        {
+            x = 1;
+        }
+        else if (newposition.x < oldposition.x && Mathf.Abs(newposition.x - oldposition.x) > Mathf.Abs(newposition.y - oldposition.y))
+        {
+            x = -1;
+        }
+        else { x = 0; }
+
+        if(newposition.y > oldposition.y)
+        {
+            y = 1;
+        }
+        else if (newposition.y < oldposition.y)
+        {
+            y = -1;
+        }
+        else { y = 0; }
+
+        if (Moving)
+        {
+
+            anim.SetFloat("X", x);
+            anim.SetFloat("Y", y);
+
+        }
+        anim.SetBool("Moving", Moving);
+
+
+    }
+}
